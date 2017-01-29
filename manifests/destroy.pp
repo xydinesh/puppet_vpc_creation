6dccbca73976002f8557143b2b81e4b2601bb2b8
Dinesh Weerapurage <dinesh.weerapurage@pearson.com>
2017-01-29 11:11:34 -0700

    Delete security group

--- manifests/destroy.pp
+++ manifests/destroy.pp
@@ -5,26 +5,36 @@
 class vpc_creation::destroy (
   $region
 ){
+  ec2_securitygroup { 'nibiru_sec_group':
+    ensure => absent,
+    region => $region
+  }~>
+
   ec2_vpc_internet_gateway { 'nibiru_igw':
     ensure => absent,
     region => $region,
   }~>
+
   ec2_vpc_subnet { 'nibiru_subnet_2':
     ensure => absent,
     region => $region,
   } ~>
+
   ec2_vpc_routetable { 'nibiru_routetable_2':
     ensure => absent,
     region => $region,
   } ~>
+
   ec2_vpc_subnet { 'nibiru_subnet_1':
     ensure => absent,
     region => $region,
   } ~>
+
   ec2_vpc_routetable { 'nibiru_routetable_1':
     ensure => absent,
     region => $region,
   } ~>
+
   ec2_vpc { 'nibiru_vpc':
     ensure => absent,
     region => $region,
